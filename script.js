e37110a218c11c836b9c7f6d2b669905c49f722d
Guilherme <guilherme44.santos.costa@gmail.com>
2023-07-04 18:03:15 -0300

    funcao alterna tabelas

--- script.js
+++ script.js
@@ -1,42 +1,25 @@
-const input = document.querySelector("#input_tarefa");
-const listaTarefas = document.querySelector(".lista_tarefas");
+const buttonTarefas = document.querySelector(".tarefas-estado-button");
+let booleanTabelas = true;
 
-input.addEventListener("keypress", (event) => {
-  if (event.key === "Enter") {
-    const tarefa = input.value.trim();
-    if (tarefa !== "") {
-      const novaTarefa = criarElementoTarefa(tarefa);
-      listaTarefas.appendChild(novaTarefa);
-      input.value = "";
-    }
+function alternaTabelas() {
+  const tabelaA = document.querySelector(".lista-tarefas");
+  const tabelaB = document.querySelector(".lista-tarefas-concluida");
+
+  
+  switch (booleanTabelas){
+    case true:
+      tabelaA.style.display = "none";
+      tabelaB.style.display = "flex";
+      break;
+    case false:
+      tabelaB.style.display = "none";
+      tabelaA.style.display = "block";
+      break;
   }
-});
 
-function criarElementoTarefa(texto) {
-  const containerDiv = document.createElement("div");
-  containerDiv.classList.add("tarefas");
-
-  const leftDiv = document.createElement("div");
-  leftDiv.classList.add("left");
-
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  checkbox.classList.add("checkbox");
-  leftDiv.appendChild(checkbox);
-
-  const descricaoTarefa = document.createElement("p");
-  descricaoTarefa.textContent = texto;
-  leftDiv.appendChild(descricaoTarefa);
-
-  containerDiv.appendChild(leftDiv);
-
-  return containerDiv;
+  if(booleanTabelas === true){
+    booleanTabelas = false;
+  }else{
+    booleanTabelas = true;
+  }
 }
-
-listaTarefas.addEventListener("change", (event) => {
-  const checkbox = event.target;
-  if (checkbox.classList.contains("checkbox")) {
-    const tarefaCompleta = checkbox.parentElement.parentElement;
-    tarefaCompleta.remove();
-  }
-});
